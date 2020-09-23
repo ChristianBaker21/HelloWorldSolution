ace929f27142fb745b135257542ef81b2d3260cc
Christian Baker <christianbaker1998@gmail.com>
2020-09-23 14:28:34 -0400

    Added display my name

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -7,6 +7,8 @@ namespace HelloWorldProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Christian.");
+            Console.WriteLine("I like Git better.");
         }
     }
 }
