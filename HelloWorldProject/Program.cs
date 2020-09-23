e592ca848a04d0c7f0dbe0593cfe0aa468a4644b
Christian Baker <christianbaker1998@gmail.com>
2020-09-23 14:13:35 -0400

    Add project files.

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorldProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+
