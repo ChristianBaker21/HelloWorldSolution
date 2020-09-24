94907902a8b20903bce11c756f428f6fbfc6bc5c
Christian Baker <christianbaker1998@gmail.com>
2020-09-24 09:26:07 -0400

    Fun With Math Project

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace HelloWorldProject
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("My name is Christian.");
-            Console.WriteLine("I like Git better.");
-        }
+        static void Main(string[] args){
+            int[] numbers = { 573, 595, 468, 191, 964 };
+            var total = 0;
+            total = total + numbers[0];
+            total = total + numbers[1];
+            total = total + numbers[2];
+            total = total + numbers[3];
+            total = total + numbers[4];
+            Console.WriteLine(total);
+           
+         }
     }
 }
 
